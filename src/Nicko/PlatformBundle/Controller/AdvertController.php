32ddf9ae0db44df484051f0a013b4ad0df84deb4
NickoM31 <nicolasmathieu31130@gmail.com>
2017-03-04 22:24:27 +0100

    Générer Url manque à afficher dans le twig

--- src/Nicko/PlatformBundle/Controller/AdvertController.php
+++ src/Nicko/PlatformBundle/Controller/AdvertController.php
@@ -6,18 +6,20 @@ namespace Nicko\PlatformBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
-  public function indexAction()
-  {
-    $content = $this
-    ->get('templating')
-    ->render('NickoPlatformBundle:Advert:index.html.twig', ['nom'=>'Kony']);
-
-    
-    return new Response($content);
-  }
+	public function viewAction(){
+		// On veut avoir l'URL de l'annonce d'id 5.
+        $url = $this->get('router')->generate(
+            'nicko_platform_view', // 1er argument : le nom de la route
+            ['id' => 5]    // 2e argument : les valeurs des paramètres
+        );
+        // $url vaut « /platform/advert/5 »
+        
+        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+	}
 }
 
